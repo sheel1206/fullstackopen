bb10dfd88131c7233ada68a209c78c48dbf30f84
Jayesh patel <sheelpatel2001@gmail.com>
2026-09-14 13:11:04 +0530

    React Changes

--- part2/temp/src/App.jsx
+++ part2/temp/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
 import Note from './components/Note'
 
-
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note = {note}/>
         )}
       </ul>
     </div>
